5ad2f7b5c8befde3917ff9616737c51df02cd5da
Trentyn Nicholas <trentynnicholas@Trentyns-MacBook-Pro.local>
2025-02-14 16:01:01 -0700

    Added same paths for .env testing

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,12 @@
 const path = require('path');
 
 // Load dotenv with environment-specific configuration
-if (process.env.NODE_ENV === 'production') {
-  require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
-  console.log('Running in Production Mode');
-} else {
-  require('dotenv').config(); // Defaults to .env in the same directory
-  console.log('Running in Development Mode');
-}
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'production'
+    ? path.resolve(__dirname, '../../../.env')  // Correct path for production
+    : path.resolve(__dirname, '../../../.env')  // Also use the same path for development for now
+});
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
